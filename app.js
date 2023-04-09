1ddef2f4b178f36bcd4023ae2881abac9c42e511
jolly01008 <jollgmll292@gmail.com>
2023-04-09 16:18:53 +0800

    feat:set body-parser and post routing

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const exphbs = require('express-handlebars')
 
 app.engine('handlebars',exphbs({defaultLayout:'main'}))
 app.set('view engine','handlebars')
-
+app.use(express.urlencoded({ extended: true })) //需載入body-parser才能抓取req.body的表單資料
 
 app.get('/', (req,res) => {
   res.render('index')
+})
+
+app.post('/', (req,res) => {
+  const Url = req.body.shortURL
+  console.log('req.body.shortUrl:' , Url)
+  res.render('index', { Url })
 })
 
 app.listen( port , () =>{
